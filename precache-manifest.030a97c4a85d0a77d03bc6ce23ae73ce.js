e71933d3a641ddeb9883f17dc73f55e745b95343
your-flat <young-do@kakao.com>
2020-07-02 23:36:55 +0900

    first commit

---
+++ precache-manifest.030a97c4a85d0a77d03bc6ce23ae73ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f15bd14f752e80dbe7eaeb4b3adbd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b40ebf2fdc809dca1064",
+    "url": "/static/css/main.e1e79a63.chunk.css"
+  },
+  {
+    "revision": "08f46d5a617e936d16f6",
+    "url": "/static/js/2.03dd0f56.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.03dd0f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40ebf2fdc809dca1064",
+    "url": "/static/js/main.c1e2f559.chunk.js"
+  },
+  {
+    "revision": "d5c7d6c6a9bd8908e823",
+    "url": "/static/js/runtime-main.e7bc551e.js"
+  },
+  {
+    "revision": "860a5176cb3e5ed34aeea1626e2ddbb4",
+    "url": "/static/media/card-back.860a5176.png"
+  }
+]);
