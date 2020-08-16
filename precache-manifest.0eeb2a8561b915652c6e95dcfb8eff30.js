ec679f78bdefcb7758c97e1d9a3ef424138e575f
your-flat <young-do@kakao.com>
2020-08-17 02:06:38 +0900

    change name of localstorage

--- precache-manifest.0eeb2a8561b915652c6e95dcfb8eff30.js
+++ precache-manifest.0eeb2a8561b915652c6e95dcfb8eff30.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "86518dfa781f4c3add818d5a94accfe6",
+    "revision": "f0bd24f8e9ac8bb3f2afe5c26024f8f2",
     "url": "/index.html"
   },
   {
-    "revision": "2a9ff01d19c7469d5d03",
+    "revision": "e27016cc82781cf16ffc",
     "url": "/static/css/main.e1e79a63.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.edd12643.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2a9ff01d19c7469d5d03",
-    "url": "/static/js/main.246d4526.chunk.js"
+    "revision": "e27016cc82781cf16ffc",
+    "url": "/static/js/main.07183edc.chunk.js"
   },
   {
     "revision": "d5c7d6c6a9bd8908e823",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c79ab5cac6c1e644f3a94522be64b35f.js"
+  "/precache-manifest.0eeb2a8561b915652c6e95dcfb8eff30.js"
 );
 
 self.addEventListener('message', (event) => {
